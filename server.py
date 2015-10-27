47395f3fd1baf39311364434a9f4a69525a1744b
Brian Tomlinson <darthlukan@gmail.com>
2015-10-27 17:19:21 +0100

    spin up a basic server and make sure ropeproject doesn't make its way into commits

---
+++ server.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, jsonify
+
+
+DEV_KEY = os.getenv('REFUGEE_ROUTE_KEY')
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(SECRET_KEY=DEV_KEY))
+
+
+@app.route('/', methods=['GET'])
+def root():
+    data = {'status': 200, 'message': 'OK!'}
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
